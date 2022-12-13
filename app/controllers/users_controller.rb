e0f7ce5012c8c73055033d644341e885e8624dcb
syunsuke sasaki <sasaki2931@gmail.com>
2022-12-13 15:17:35 +0900

    リダイレクト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
-        # 保存の成功した場合の処理
+        redirect_to "http://localhost:3000/facebooks"
       else
         render :new
       end
