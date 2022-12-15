32208c9bccc1d5a8485dc3468714a173841c7122
syunsuke sasaki <sasaki2931@gmail.com>
2022-12-15 17:28:19 +0900

    エラー解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
         render :new
       end
     end
+
+    def show
+      @user = User.find(params[:id])
+    end
     
     private
     
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :facebooks
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create,:show]
 end
